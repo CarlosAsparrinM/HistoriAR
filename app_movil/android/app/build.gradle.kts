e4f96e2299124badda358dd511042f2d82f71255
Carlos Asparrin Martin <150694554+SujetoDelta-XXI@users.noreply.github.com>
2026-06-01 11:05:27 -0500

    feat: configure Android build settings including release signing and core library desugaring

--- app_movil/android/app/build.gradle.kts
+++ app_movil/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ val isReleaseTask = gradle.startParameter.taskNames.any {
 android {
     namespace = "com.historiar.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -70,6 +70,7 @@ android {
             )
         }
     }
+
 }
 
 flutter {
